e263349b66db4ad377ec2021d699363f8230202f
ManuelMBaumann <manuelmbaumann@gmail.com>
2017-11-06 16:49:12 +0100

    upd ex2

--- machine-learning-ex2/ex2-python/ex2.py
+++ machine-learning-ex2/ex2-python/ex2.py
@@ -1,151 +1,170 @@
-%% Machine Learning Online Class - Exercise 2: Logistic Regression
-%
-%  Instructions
-%  ------------
-% 
-%  This file contains code that helps you get started on the logistic
-%  regression exercise. You will need to complete the following functions 
-%  in this exericse:
-%
-%     sigmoid.m
-%     costFunction.m
-%     predict.m
-%     costFunctionReg.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
+## Machine Learning Online Class - Exercise 2: Logistic Regression
+#
+#  Instructions
+#  ------------
+# 
+#  This file contains code that helps you get started on the logistic
+#  regression exercise. You will need to complete the following functions 
+#  in this exericse:
+#
+#     sigmoid.py
+#     costFunction.py
+#     predict.py
+#     costFunctionReg.py
+#
+#  For this exercise, you will not need to change any code in this file,
+#  or any other files other than those mentioned above.
+#
 
-%% Initialization
-clear ; close all; clc
+## Load Data
+#  The first two columns contains the exam scores and the third column
+#  contains the label.
 
-%% Load Data
-%  The first two columns contains the exam scores and the third column
-%  contains the label.
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
-data = load('ex2data1.txt');
-X = data(:, [1, 2]); y = data(:, 3);
+def plotData(X,y):
+    # Find Indices of Positive and Negative Examples
+    pos = np.where(y==1)
+    neg = np.where(y==0)
+    plt.plot(X[pos, 0], X[pos, 1], 'k+')
+    plt.plot(X[neg, 0], X[neg, 1], 'yo')
+    plt.xlabel('Exam 1 score')
+    plt.ylabel('Exam 2 score')
+    plt.legend(['Admitted', 'Not admitted'])
+    plt.show()
+    
+sigmoid = lambda z: 1.0/(1.0+np.exp(-z))
 
-%% ==================== Part 1: Plotting ====================
-%  We start the exercise by first plotting the data to understand the 
-%  the problem we are working with.
+def costFunction(theta, X, y):
+    m = len(y)
+    cost = 0.0
+    for i in range(m):
+        cost = cost + (-y[i]*np.log(sigmoid(np.dot(theta.T,X[i,:]))) - (1-y[i])*np.log(1-sigmoid(np.dot(theta.T,X[i,:]))))
+    return 1.0/m*cost
 
-fprintf(['Plotting data with + indicating (y = 1) examples and o ' ...
-         'indicating (y = 0) examples.\n']);
+def costFunction_der(theta, X, y):
+    m = len(y)
+    grad = 0.0*theta
+    for i in range(m):
+        for j in range(len(theta)):
+            grad[j] = grad[j] + ( sigmoid(np.dot(theta.T,X[i,:])) -y[i] )*X[i,j]
+    return 1.0/m*grad
+    
 
-plotData(X, y);
+data = data = np.loadtxt('ex2data1.txt')
+X = data[:, :2]
+y = data[:, -1]
 
-% Put some labels 
-hold on;
-% Labels and Legend
-xlabel('Exam 1 score')
-ylabel('Exam 2 score')
+### ==================== Part 1: Plotting ====================
+##  We start the exercise by first plotting the data to understand the 
+##  the problem we are working with.
+print(['Plotting data with + indicating (y = 1) examples and o indicating (y = 0) examples.'])
+plotData(X, y)
+input("Press Enter to continue...")
 
-% Specified in plot order
-legend('Admitted', 'Not admitted')
-hold off;
+### ============ Part 2: Compute Cost and Gradient ============
+##  In this part of the exercise, you will implement the cost and gradient
+##  for logistic regression. You need to complete the code in 
+##  costFunction.py
 
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+##  Setup the data matrix appropriately, and add ones for the intercept term
+m, n = X.shape
 
+## Add intercept term to x and X_test
+X = np.ones((m,n+1), dtype=float)
+X[:,1:] = data[:, :2]
 
-%% ============ Part 2: Compute Cost and Gradient ============
-%  In this part of the exercise, you will implement the cost and gradient
-%  for logistic regression. You neeed to complete the code in 
-%  costFunction.m
+## Initialize fitting parameters
+initial_theta = np.zeros((n+1,1))
 
-%  Setup the data matrix appropriately, and add ones for the intercept term
-[m, n] = size(X);
+## Compute and display initial cost and gradient
+cost = costFunction(initial_theta, X, y)
+grad = costFunction_der(initial_theta, X, y)
 
-% Add intercept term to x and X_test
-X = [ones(m, 1) X];
+print(['Cost at initial theta (zeros): ', cost])
+print('Expected cost (approx): 0.693\n')
+print('Gradient at initial theta (zeros): \n')
+print([' #f \n', grad])
+print('Expected gradients (approx):\n -0.1000\n -12.0092\n -11.2628\n')
 
-% Initialize fitting parameters
-initial_theta = zeros(n + 1, 1);
+## Compute and display cost and gradient with non-zero theta
+test_theta = np.array([-24, 0.2, 0.2])
+cost = costFunction(test_theta, X, y)
+grad = costFunction_der(test_theta, X, y)
 
-% Compute and display initial cost and gradient
-[cost, grad] = costFunction(initial_theta, X, y);
+print(['Cost at test theta: ', cost])
+print('Expected cost (approx): 0.218')
+print('Gradient at test theta: ')
+print(' #f ', grad)
+print('Expected gradients (approx):\n 0.043\n 2.566\n 2.647\n')
 
-fprintf('Cost at initial theta (zeros): %f\n', cost);
-fprintf('Expected cost (approx): 0.693\n');
-fprintf('Gradient at initial theta (zeros): \n');
-fprintf(' %f \n', grad);
-fprintf('Expected gradients (approx):\n -0.1000\n -12.0092\n -11.2628\n');
-
-% Compute and display cost and gradient with non-zero theta
-test_theta = [-24; 0.2; 0.2];
-[cost, grad] = costFunction(test_theta, X, y);
-
-fprintf('\nCost at test theta: %f\n', cost);
-fprintf('Expected cost (approx): 0.218\n');
-fprintf('Gradient at test theta: \n');
-fprintf(' %f \n', grad);
-fprintf('Expected gradients (approx):\n 0.043\n 2.566\n 2.647\n');
-
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+input("Press Enter to continue...")
 
 
-%% ============= Part 3: Optimizing using fminunc  =============
-%  In this exercise, you will use a built-in function (fminunc) to find the
-%  optimal parameters theta.
+### ============= Part 3: Optimizing using fminunc  =============
+##  In this exercise, you will use a built-in function (fminunc) to find the
+##  optimal parameters theta.
 
-%  Set options for fminunc
-options = optimset('GradObj', 'on', 'MaxIter', 400);
+##  Set options for fminunc
+#options = optimset('GradObj', 'on', 'MaxIter', 400);
 
-%  Run fminunc to obtain the optimal theta
-%  This function will return theta and the cost 
-[theta, cost] = ...
-	fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
+##  Run fminunc to obtain the optimal theta
+##  This function will return theta and the cost 
+#[theta, cost] = ...
+	#fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
 
-% Print theta to screen
-fprintf('Cost at theta found by fminunc: %f\n', cost);
-fprintf('Expected cost (approx): 0.203\n');
-fprintf('theta: \n');
-fprintf(' %f \n', theta);
-fprintf('Expected theta (approx):\n');
-fprintf(' -25.161\n 0.206\n 0.201\n');
+res = minimize()
 
-% Plot Boundary
-plotDecisionBoundary(theta, X, y);
 
-% Put some labels 
-hold on;
-% Labels and Legend
-xlabel('Exam 1 score')
-ylabel('Exam 2 score')
+## Print theta to screen
+print(['Cost at theta found by fminunc: ', cost])
+print('Expected cost (approx): 0.203')
+print('theta:')
+print([' #f ', theta])
+print('Expected theta (approx):')
+print('-25.161 0.206 0.201')
 
-% Specified in plot order
-legend('Admitted', 'Not admitted')
-hold off;
+## Plot Boundary
+#plotDecisionBoundary(theta, X, y);
 
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+## Put some labels 
+#hold on;
+## Labels and Legend
+#xlabel('Exam 1 score')
+#ylabel('Exam 2 score')
 
-%% ============== Part 4: Predict and Accuracies ==============
-%  After learning the parameters, you'll like to use it to predict the outcomes
-%  on unseen data. In this part, you will use the logistic regression model
-%  to predict the probability that a student with score 45 on exam 1 and 
-%  score 85 on exam 2 will be admitted.
-%
-%  Furthermore, you will compute the training and test set accuracies of 
-%  our model.
-%
-%  Your task is to complete the code in predict.m
+## Specified in plot order
+#legend('Admitted', 'Not admitted')
+#hold off;
 
-%  Predict probability for a student with score 45 on exam 1 
-%  and score 85 on exam 2 
+input("Press Enter to continue...")
 
-prob = sigmoid([1 45 85] * theta);
-fprintf(['For a student with scores 45 and 85, we predict an admission ' ...
-         'probability of %f\n'], prob);
-fprintf('Expected value: 0.775 +/- 0.002\n\n');
+### ============== Part 4: Predict and Accuracies ==============
+##  After learning the parameters, you'll like to use it to predict the outcomes
+##  on unseen data. In this part, you will use the logistic regression model
+##  to predict the probability that a student with score 45 on exam 1 and 
+##  score 85 on exam 2 will be admitted.
+##
+##  Furthermore, you will compute the training and test set accuracies of 
+##  our model.
+##
+##  Your task is to complete the code in predict.m
 
-% Compute accuracy on our training set
-p = predict(theta, X);
+##  Predict probability for a student with score 45 on exam 1 
+##  and score 85 on exam 2 
 
-fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
-fprintf('Expected accuracy (approx): 89.0\n');
-fprintf('\n');
+#prob = sigmoid([1 45 85] * theta);
+#fprintf(['For a student with scores 45 and 85, we predict an admission ' ...
+         #'probability of #f\n'], prob);
+#fprintf('Expected value: 0.775 +/- 0.002\n\n');
+
+## Compute accuracy on our training set
+#p = predict(theta, X);
+
+#fprintf('Train Accuracy: #f\n', mean(double(p == y)) * 100);
+#fprintf('Expected accuracy (approx): 89.0\n');
+#fprintf('\n');
 
 
